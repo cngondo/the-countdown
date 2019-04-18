15a0aaa41f29756dd0ccf55e01fc8da61a071611
cngondo <devngondo@gmail.com>
2019-04-17 19:31:20 -0500

    feat: Create barebones jsx of the app
    Add sample components
    Add emmet extensions for writing jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import Progress from "./Progress";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dateToday: 0
+    };
+  }
   render() {
     return (
       <div className="App">
-        <p> Some random component </p>
+        <h2>Some Title of a Timer</h2>
+        <Progress />
       </div>
     );
   }
